649c67527d81ad1786c19238aa89e25a8b9add15
AlRashiIssa <rashidissa2001@icloud.com>
2025-02-12 10:00:02 +0300

    Finsish V 8

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -2,4 +2,9 @@ from enum import Enum
 
 class ResponseSignal(Enum):
 
+    FILE_VALIDATED_SUCCESS = "file_validate_successfully"
     FILE_TYPE_NOT_SUPPORTED = "file_type_not_support"
+    FILE_SIZE_EXCEED = "file_size_exceeded"
+    FILE_UPLOAD_SUCCESS = "file_upload_success"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
+
